b87be2dfc95910d959a2a8c00937b929e827d73a
Scott Morris <scott.r.morris@gmail.com>
2020-09-04 22:22:24 -0400

    Removed hardcoded dates in events API

--- server/src/icloud/events.js
+++ server/src/icloud/events.js
@@ -1,8 +1,18 @@
+// Libraries.
+
+const moment = require('moment');
+
+// Private.
+
+const beginningOfLastMonth = () =>
+  moment().subtract(1, 'month').startOf('month').format('YYYY-MM-DD');
+const endOfNextMonth = () => moment().add(1, 'month').endOf('month').format('YYYY-MM-DD');
+
 // Public.
 
 const middleware = (iCloudSession, options) => async (req, res, next) => {
-  const startDate = req.query.startDate || '2020-08-01';
-  const endDate = req.query.endDate || '2020-08-31';
+  const startDate = req.query.startDate || beginningOfLastMonth();
+  const endDate = req.query.endDate || endOfNextMonth();
   const events = await iCloudSession.Calendar.getEvents(startDate, endDate);
   res.json(events);
 };

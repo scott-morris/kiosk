49958d839563c8a1a18ea69ffae4dc453f1981d9
Scott Morris <scott.r.morris@gmail.com>
2020-09-05 23:20:40 -0400

    Added multiple views

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
+// Base Libraries.
+
+import React, { useState } from 'react';
+
+// View Management.
+
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
+// Components.
+
 import Calendar from './components/Calendar';
 import DateTime from './components/DateTime';
+
+// Styles.
+
 import './App.scss';
 
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
 function App() {
+  const [index, setIndex] = useState(0);
+
   return (
     <div className="App">
-      <Calendar />
-      <DateTime />
+      <AutoPlaySwipeableViews index={index} onChangeIndex={setIndex}>
+        <div>
+          <Calendar />
+          <DateTime />
+        </div>
+        <div>
+          <DateTime />
+        </div>
+      </AutoPlaySwipeableViews>
     </div>
   );
 }

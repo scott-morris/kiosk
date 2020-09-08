ba008b0c4e8ebfa4651fef11700ee02ef4f9536d
Scott Morris <scott.r.morris@gmail.com>
2020-09-07 23:49:07 -0400

    Allow for multiple icons

--- client/src/components/Weather/Icon.js
+++ client/src/components/Weather/Icon.js
@@ -1,19 +1,28 @@
 // Libraries.
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 // Public.
 
-const Icon = ({ weather, iconClass = '' }) => (
-  <div className={`wc-icon ${iconClass}`}>
-    {weather.map(({ icon, description }, index) => (
-      <img
-        key={`wc-icon${index}`}
-        src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
-        alt={description}
-      />
-    ))}
-  </div>
-);
+const Icon = ({ weather, className = '', iconClass = '' }) => {
+  const [index, setIndex] = useState(0);
+  const [entry, setEntry] = useState({});
+
+  useEffect(() => {
+    setEntry(weather[index]);
+
+    setTimeout(() => {
+      const newIndex = index + 1 < weather.length ? index + 1 : 0;
+
+      setIndex(newIndex);
+    }, 2000);
+  }, [index, weather, className, iconClass]);
+
+  return (
+    <div className={['wc-icon', className, iconClass].join(' ')}>
+      <img src={`http://openweathermap.org/img/wn/${entry.icon}@2x.png`} alt={entry.description} />
+    </div>
+  );
+};
 
 export default Icon;
